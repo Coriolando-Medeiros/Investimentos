9d90ccbdb432255ba724d25091d5f62e9e1606d1
Coriolando-Medeiros <coriolando.medeiros@gmail.com>
2024-09-18 05:38:19 -0300

    feat(extrair_campos): criar a funcionalidade de extrair os dados do arquivo txt

--- carteira.rb
+++ carteira.rb
@@ -1,7 +1,9 @@
+require 'date'
+
 class Carteira
 
     def initialize
-        @clientes = []
+        @cliente = []
     end
 
 
@@ -53,9 +55,57 @@ class Carteira
         end
     end
 
+    def extrair_campos(linha)
+        
+        # Expressões regulares para capturar os campos
+        investimento = linha.match(/Investimento:\s*(.*?)(?=\s*,|\z)/)[1]
+        retorno = linha.match(/Retorno:\s*([\d.]+)\s*%/)[1].to_f
+        aporte_inicial = linha.match(/Aporte inicial:\s*R\$ ([\d.,]+)/)[1].gsub(',', '.').to_f
+        data_str = linha.match(/Data:\s*([\d\/]+)/)[1]
+        data = Date.strptime(data_str, "%d/%m/%Y")
+      
+        { investimento: investimento, retorno: retorno, aporte_inicial: aporte_inicial, data: data }
+    end
+
     def calcular_retorno
+        puts "Calcular retorno"
+        if File.exist?("investimentos.txt") && !File.zero?("investimentos.txt")
+            print "Nome: "
+            nome = gets.chomp.upcase
+            @clientes = File.readlines("investimentos.txt")
+
+            cliente_nome = @clientes.select { |cliente| cliente.include?(nome)}
+
+            cliente_nome.map!(&:chomp)
+
+            #puts cliente_nome
+
+            datas = []
+
+            cliente_nome.each do |cliente|
+                elementos = cliente.split(" ")  # Divide a linha em partes
+                data_str = elementos.last       # Pega o último elemento (a data)
+
+                # Converter a string de data para um objeto Date e armazenar no array
+                data = Date.strptime(data_str, "%d/%m/%Y")  # Ajuste o formato conforme necessário
+                datas << data
+            end
+
+            # Exibir as datas convertidas
+            puts "Datas: #{datas}"
+
+            # Exemplo de cálculo com datas
+            # Subtrair a primeira data da segunda (se houver mais de uma)
+            if datas.length > 1
+                diferenca = datas[1] - datas[0]  # Diferença em dias
+                puts "Diferença entre as duas primeiras datas: #{diferenca} dias"
+            end
+
+        else
+            puts "Arquivo inexistente!"
+        end
     end
 end
 
 investir = Carteira.new
-investir.acompanhar_investimentos
+investir.mostrar
